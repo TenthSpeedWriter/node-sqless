80e850246ee8b57e958afc2d88faf5ab6634b8b4
Chef <addseale2@gmail.com>
2015-10-28 17:31:45 +0000

    Adds rough MySQL code and testing

--- src/mysql-crud-Table.js
+++ src/mysql-crud-Table.js
@@ -1,14 +1,47 @@
-var Bro = require("brototype");
+var Bro = require("brototype"),
+    q = require("q");
 
-var Table = function (table_name, connection) {
-    this.table_name = table_name;
+var Table = function (table_name, connection_method) {
+    var _self = this,
+        deferred = q.defer(),
+        mysql = connection_method.mysql,
+        config = connection_method.config;
+        
+    this.Query_Promise = function () {
+        this.commafied_keys = function (keys) {
+            
+        };
+        
+        this.equalized_keyvals = function(data) {
+            
+        };
+    };
     
-    this.query = function () {
-        
-    };
-    this.query.prototype = {
-        
-    };
+    var table_description_connection = mysql.createConnection(config);
+    table_description_connection.query(("DESCRIBE " + table_name), function (err, rows, fields) {
+        if (err) { 
+            table_description_connection.end();
+            deferred.reject(Bro(_self).comeAtMe({
+                err: err
+            }));
+        } else {
+            _self.data_structure = {};
+            rows.map(function (row) {
+                _self.data_structure[row.Field] = {
+                    Type: row.Type,
+                    Null: row.Null,
+                    Key: row.Key,
+                    Default: row.Default,
+                    Extra: row.Extra
+                };
+            });
+            
+            table_description_connection.end();
+            deferred.resolve(_self);
+        }
+    });
+    
+    return deferred.promise;
 };
 
 module.exports = Table;
--- test/mysql-crud-Table-test.js
+++ test/mysql-crud-Table-test.js
@@ -1,7 +1,13 @@
 var Table = require("../src/mysql-crud-Table.js"),
     expect = require("chai").expect,
     mysql = require("mysql"),
-    config = JSON.parse(require("fs").readFileSync('mysql-crud-test-config', 'utf-8'));
+    Bro = require("brototype"),
+    config = {
+        "host": "localhost",
+        "user": "tenthspeedwriter",
+        "database": "sqless_test",
+        "tables": "*"
+    };
     
 function expect_no_errors(err) {
     console.err(err);
@@ -9,15 +15,53 @@ function expect_no_errors(err) {
 }
 
 describe("Table", function () {
-    var connection;
-    
+    var table;
     beforeEach(function (done) {
-        connection = mysql.create_connection(config);
-        connection.connect();
-        done();
+        Table("people", {
+                mysql: mysql,
+                config: config
+            })
+            .then(function (new_table) {
+                table = new_table;
+                done();
+            });
     });
     
-    afterEach(connection.end);
+    it("initializes with a .data_structure, a description of the table", function () {
+       var data_structure = table.data_structure;
+       expect(data_structure).to.be.ok;
+    });
+    
+    describe(".data_structure", function () {
+        var data_structure = table.data_structure;
+       
+        it("has named child objects, at least one of which is 'peopleID'", function () {
+            expect(data_structure.peopleID).to.be.ok;
+            expect(typeof data_structure.peopleID).to.be.object;
+        });
+        
+        describe(".peopleID", function () {
+            ['Type', 'Null', 'Key', 'Default', 'Extra'].map(function (field_name) {
+                it(("has attribute " + field_name), function () {
+                    expect(data_structure.peopleID[field_name]).to.be.ok;
+                });
+            });
+        });
+    })
+});
+
+/*
+describe("Table", function () {
+    var connection;
+    
+    beforeEach(function () {
+        connection = mysql.create_connection(config);
+        connection.connect();
+    });
+    
+    afterEach(function () {
+        connection.end();
+    });
     
     it("throws an error if not given a valid connection", function () {
         expect(Table("bad_table", null)).to.throw("BAD_CONNECTION_OBJECT");
@@ -56,3 +100,4 @@ describe("Table", function () {
         });
     });
 });
+*/
